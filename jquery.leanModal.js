5379b94b05d280bbdc7958b5a42b0a2479e4096f
Gabriel Osorio <gabriel.osorio@neonroots.com>
2013-08-29 11:06:10 -0300

    Improved indentation consistency and changed it to two spaces.

--- jquery.leanModal.js
+++ jquery.leanModal.js
@@ -1,73 +1,73 @@
 (function($) {
 
-    $.fn.extend({
+  $.fn.extend({
 
-        leanModal: function(options) {
+    leanModal: function(options) {
 
-            var defaults = {
-                top: 100,
-                overlay: 0.5,
-                closeButton: null
-            }
+      var defaults = {
+          top: 100,
+          overlay: 0.5,
+          closeButton: null
+      }
 
-            var overlay = $("<div id='lean_overlay'></div>");
+      var overlay = $("<div id='lean_overlay'></div>");
 
-            $("body").append(overlay);
+      $("body").append(overlay);
 
-            options =  $.extend(defaults, options);
+      options =  $.extend(defaults, options);
 
-            return this.each(function() {
+      return this.each(function() {
 
-                var o = options;
+        var o = options;
 
-                $(this).click(function(e) {
+        $(this).click(function(e) {
 
-                var modal_id = $(this).attr("href");
+          var modal_id = $(this).attr("href");
 
-        $("#lean_overlay").click(function() {
-                     close_modal(modal_id);
-                });
+          $("#lean_overlay").click(function() {
+             close_modal(modal_id);
+          });
 
-                $(o.closeButton).click(function() {
-                     close_modal(modal_id);
-                });
+          $(o.closeButton).click(function() {
+             close_modal(modal_id);
+          });
 
-                var modal_height = $(modal_id).outerHeight();
-              var modal_width = $(modal_id).outerWidth();
+          var modal_height = $(modal_id).outerHeight();
+          var modal_width = $(modal_id).outerWidth();
 
-            $('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
+          $('#lean_overlay').css({ 'display' : 'block', opacity : 0 });
 
-            $('#lean_overlay').fadeTo(200,o.overlay);
+          $('#lean_overlay').fadeTo(200,o.overlay);
 
-            $(modal_id).css({
+          $(modal_id).css({
 
-              'display' : 'block',
-              'position' : 'fixed',
-              'opacity' : 0,
-              'z-index': 11000,
-              'left' : 50 + '%',
-              'margin-left' : -(modal_width/2) + "px",
-              'top' : o.top + "px"
+            'display' : 'block',
+            'position' : 'fixed',
+            'opacity' : 0,
+            'z-index': 11000,
+            'left' : 50 + '%',
+            'margin-left' : -(modal_width/2) + "px",
+            'top' : o.top + "px"
 
-            });
+          });
 
-            $(modal_id).fadeTo(200,1);
+          $(modal_id).fadeTo(200,1);
 
-                e.preventDefault();
+          e.preventDefault();
 
-                });
+        });
 
-            });
+      });
 
       function close_modal(modal_id) {
 
-            $("#lean_overlay").fadeOut(200);
+        $("#lean_overlay").fadeOut(200);
 
-            $(modal_id).css({ 'display' : 'none' });
+        $(modal_id).css({ 'display' : 'none' });
 
       }
 
-        }
-    });
+    }
+  });
 
 })(jQuery);
